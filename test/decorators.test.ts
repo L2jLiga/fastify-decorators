0d9ca0182159108145e1f2dc04d9abfe2c075114
Andrey Chalkin <andrey.chalkin@sidenis.com>
2019-05-14 10:16:07 +0700

    Added tests for decorators

---
+++ test/decorators.test.ts
@@ -0,0 +1,75 @@
+/**
+ * @license
+ * Copyright Andrey Chalkin <L2jLiga@gmail.com> (https://github.com/L2jLiga). All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/L2jLiga/fastify-decorators/blob/master/LICENSE
+ */
+
+import { ALL, DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT } from '../lib/decorators';
+
+const tap = require('tap');
+
+tap.test('ALL decorator should patch sample class', async (t: any) => {
+    class A {}
+
+    ALL({url: '/'})(A);
+
+    t.match(typeof (A as any).register, 'function')
+});
+
+tap.test('DELETE decorator should patch sample class', async (t: any) => {
+    class A {}
+
+    DELETE({url: '/'})(A);
+
+    t.match(typeof (A as any).register, 'function')
+});
+
+tap.test('GET decorator should patch sample class', async (t: any) => {
+    class A {}
+
+    GET({url: '/'})(A);
+
+    t.match(typeof (A as any).register, 'function')
+});
+
+tap.test('HEAD decorator should patch sample class', async (t: any) => {
+    class A {}
+
+    HEAD({url: '/'})(A);
+
+    t.match(typeof (A as any).register, 'function')
+});
+
+tap.test('OPTIONS decorator should patch sample class', async (t: any) => {
+    class A {}
+
+    OPTIONS({url: '/'})(A);
+
+    t.match(typeof (A as any).register, 'function')
+});
+
+tap.test('PATCH decorator should patch sample class', async (t: any) => {
+    class A {}
+
+    PATCH({url: '/'})(A);
+
+    t.match(typeof (A as any).register, 'function')
+});
+
+tap.test('POST decorator should patch sample class', async (t: any) => {
+    class A {}
+
+    POST({url: '/'})(A);
+
+    t.match(typeof (A as any).register, 'function')
+});
+
+tap.test('PUT decorator should patch sample class', async (t: any) => {
+    class A {}
+
+    PUT({url: '/'})(A);
+
+    t.match(typeof (A as any).register, 'function')
+});
---
+++ test/sample-app.test.ts
@@ -0,0 +1,36 @@
+/**
+ * @license
+ * Copyright Andrey Chalkin <L2jLiga@gmail.com> (https://github.com/L2jLiga). All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/L2jLiga/fastify-decorators/blob/master/LICENSE
+ */
+
+import { GET } from '../lib/decorators';
+import { RequestHandler } from '../lib/interfaces';
+import fastify = require('fastify');
+
+const tap = require('tap');
+
+@GET({
+    url: '/sample',
+    options: {}
+})
+class SampleAppTest extends RequestHandler {
+    async handle() {
+        return {message: 'OK!'};
+    }
+}
+
+tap.test('Should work', async (t: any) => {
+    const instance = fastify();
+
+    SampleAppTest.register(instance);
+
+    const res = await instance.inject({
+        method: 'GET',
+        url: '/sample'
+    });
+
+    t.match(res.payload, `{"message":"OK!"}`);
+});
