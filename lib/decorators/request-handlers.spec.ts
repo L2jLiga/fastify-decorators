4adc83ceb930229bc4e7adc1e85f9b0654f82ea2
Andrey Chalkin <L2jLiga@gmail.com>
2020-09-07 12:28:52 +0700

    chore: improve code coverage

---
+++ lib/decorators/request-handlers.spec.ts
@@ -0,0 +1,173 @@
+import { RouteShorthandOptions } from 'fastify';
+import { CREATOR } from '../symbols';
+import { ALL, GET, POST, DELETE, HEAD, OPTIONS, PATCH, PUT } from './request-handlers';
+
+describe('Decorators: request handlers', () => {
+    it('should instantiate ALL handler', () => {
+        class Instance {
+            constructor(private url: string, private options: RouteShorthandOptions) {
+            }
+
+            all(url: string, options: RouteShorthandOptions) {
+                expect(url).toEqual(this.url);
+                expect(options).toEqual(this.options);
+            }
+        }
+        class Handler {}
+
+        const instance = new Instance('/', {});
+        const decorate = ALL('/');
+
+        decorate(Handler);
+
+        // @ts-expect-error created implicitly by decorate
+        Handler[CREATOR].register(instance);
+    });
+
+    it('should instantiate GET handler', () => {
+        class Instance {
+            constructor(private url: string, private options: RouteShorthandOptions) {
+            }
+
+            get(url: string, options: RouteShorthandOptions) {
+                expect(url).toEqual(this.url);
+                expect(options).toEqual(this.options);
+            }
+        }
+        class Handler {}
+
+        const instance = new Instance('/', {});
+        const decorate = GET('/');
+
+        decorate(Handler);
+
+        // @ts-expect-error created implicitly by decorate
+        Handler[CREATOR].register(instance);
+    });
+
+    it('should instantiate POST handler', () => {
+        class Instance {
+            constructor(private url: string, private options: RouteShorthandOptions) {
+            }
+
+            post(url: string, options: RouteShorthandOptions) {
+                expect(url).toEqual(this.url);
+                expect(options).toEqual(this.options);
+            }
+        }
+        class Handler {}
+
+        const instance = new Instance('/', {});
+        const decorate = POST('/');
+
+        decorate(Handler);
+
+        // @ts-expect-error created implicitly by decorate
+        Handler[CREATOR].register(instance);
+    });
+
+    it('should instantiate DELETE handler', () => {
+        class Instance {
+            constructor(private url: string, private options: RouteShorthandOptions) {
+            }
+
+            delete(url: string, options: RouteShorthandOptions) {
+                expect(url).toEqual(this.url);
+                expect(options).toEqual(this.options);
+            }
+        }
+        class Handler {}
+
+        const instance = new Instance('/', {});
+        const decorate = DELETE('/');
+
+        decorate(Handler);
+
+        // @ts-expect-error created implicitly by decorate
+        Handler[CREATOR].register(instance);
+    });
+
+    it('should instantiate HEAD handler', () => {
+        class Instance {
+            constructor(private url: string, private options: RouteShorthandOptions) {
+            }
+
+            head(url: string, options: RouteShorthandOptions) {
+                expect(url).toEqual(this.url);
+                expect(options).toEqual(this.options);
+            }
+        }
+        class Handler {}
+
+        const instance = new Instance('/', {});
+        const decorate = HEAD('/');
+
+        decorate(Handler);
+
+        // @ts-expect-error created implicitly by decorate
+        Handler[CREATOR].register(instance);
+    });
+
+    it('should instantiate OPTIONS handler', () => {
+        class Instance {
+            constructor(private _url: string, private _options: RouteShorthandOptions) {
+            }
+
+            options(url: string, options: RouteShorthandOptions) {
+                expect(url).toEqual(this._url);
+                expect(options).toEqual(this._options);
+            }
+        }
+        class Handler {}
+
+        const instance = new Instance('/', {});
+        const decorate = OPTIONS('/');
+
+        decorate(Handler);
+
+        // @ts-expect-error created implicitly by decorate
+        Handler[CREATOR].register(instance);
+    });
+
+    it('should instantiate PATCH handler', () => {
+        class Instance {
+            constructor(private url: string, private options: RouteShorthandOptions) {
+            }
+
+            patch(url: string, options: RouteShorthandOptions) {
+                expect(url).toEqual(this.url);
+                expect(options).toEqual(this.options);
+            }
+        }
+        class Handler {}
+
+        const instance = new Instance('/', {});
+        const decorate = PATCH('/');
+
+        decorate(Handler);
+
+        // @ts-expect-error created implicitly by decorate
+        Handler[CREATOR].register(instance);
+    });
+
+    it('should instantiate PUT handler', () => {
+        class Instance {
+            constructor(private url: string, private options: RouteShorthandOptions) {
+            }
+
+            put(url: string, options: RouteShorthandOptions) {
+                expect(url).toEqual(this.url);
+                expect(options).toEqual(this.options);
+            }
+        }
+        class Handler {}
+
+        const instance = new Instance('/', {});
+        const decorate = PUT('/');
+
+        decorate(Handler);
+
+        // @ts-expect-error created implicitly by decorate
+        Handler[CREATOR].register(instance);
+    });
+});
---
+++ lib/testing/mocks-manager.spec.ts
@@ -0,0 +1,14 @@
+import { Injectables } from '../interfaces/injectable-class';
+import { wrapInjectable } from '../utils/wrap-injectable';
+import { MocksManager } from './mocks-manager';
+
+describe('Testing: mocks manager', () => {
+    it('should create injectables map from another one', () => {
+        const injectables: Injectables = new Map([['a', wrapInjectable({})]]);
+
+        const created = MocksManager.create(injectables);
+
+        expect(created).toEqual(injectables);
+        expect(created).not.toBe(injectables);
+    });
+});
