a3afd40c76a775bac5a65197dbf88dbe15be0111
Andrey Chalkin <L2jLiga@gmail.com>
2020-11-01 20:07:14 +0700

    chore: updated README patcher

--- patch-readme.cjs
+++ patch-readme.cjs
@@ -1,10 +1,11 @@
 const fs = require('fs');
-const path = require('path');
+const path = require('path').posix;
 
 const packageJson = require('./dist/fastify-decorators/package');
+const baseUrl = `https://github.com/L2jLiga/fastify-decorators/blob/v${packageJson.version}/`;
 
 const readme = path.join('dist', packageJson.name, 'README.md');
 const encoding = 'utf-8';
 const content = fs.readFileSync(readme, encoding);
-const newContent = content.replace(new RegExp(']: ./', 'g'), `]: https://github.com/L2jLiga/fastify-decorators/blob/v${packageJson.version}/lib/`);
+const newContent = content.replace(/]: (.\/.+)/g, (match, file) => `]: ${baseUrl}${path.relative(__dirname, path.join(__dirname, 'lib', file))}`);
 fs.writeFileSync(readme, newContent, encoding);
