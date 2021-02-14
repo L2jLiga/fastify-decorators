a35dc2fe593ecf3c63b394966e6b256b892f6006
Andrey Chalkin <L2jLiga@gmail.com>
2021-02-14 15:39:37 +0700

    chore(CI): added Jest GH actions reporter

--- jest.examples.config.js
+++ jest.examples.config.js
@@ -3,21 +3,14 @@ module.exports = {
   rootDir: '../..',
   testEnvironment: './jest.environment.cjs',
   collectCoverage: true,
-  coverageReporters: ['text', 'lcov'],
+  coverageReporters: process.env.CI ? ['text', 'lcov', 'jest-github-actions-reporter'] : ['text'],
   globals: {
     'ts-jest': {
       tsconfig: './tsconfig.spec.json',
     },
   },
   resolver: 'jest-ts-webcompat-resolver',
-  collectCoverageFrom: [
-    'lib/**/*.ts',
-    '!lib/plugins/*.ts',
-    '!lib/**/*.mock.ts',
-    '!lib/**/*.test.ts',
-    '!lib/**/*.spec.ts',
-    '!**/.rollup.cache/**',
-  ],
+  collectCoverageFrom: ['lib/**/*.ts', '!lib/plugins/*.ts', '!lib/**/*.mock.ts', '!lib/**/*.test.ts', '!lib/**/*.spec.ts', '!**/.rollup.cache/**'],
   moduleNameMapper: {
     '^fastify-decorators$': '<rootDir>/lib/index.ts',
     '^fastify-decorators/(.*)$': '<rootDir>/lib/$1',
