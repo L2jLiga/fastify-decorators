1c1e153a7c2b137e03c0c737e739c3c158be34cb
Andrey Chalkin <L2jLiga@gmail.com>
2021-04-29 12:01:19 +0700

    fix: patch logo url for npm

--- patch-readme.cjs
+++ patch-readme.cjs
@@ -3,12 +3,15 @@ const path = require('path').posix;
 
 const packageJson = require('./dist/fastify-decorators/package');
 const baseUrl = `https://github.com/L2jLiga/fastify-decorators/blob/v${packageJson.version}/`;
+const rawUrl = `https://raw.githubusercontent.com/L2jLiga/fastify-decorators/v${packageJson.version}/`;
 
 const readme = path.join(__dirname, 'dist', packageJson.name, 'README.md');
 const encoding = 'utf-8';
 const content = fs.readFileSync(readme, encoding);
-const newContent = content.replace(
-  /]: (.\/.+)/g,
-  (match, file) => `]: ${baseUrl}${path.relative(__dirname, path.join(__dirname, file))}`,
-);
+const newContent = content
+  .replace(/]: (\.\/.+)/g, (match, file) => `]: ${baseUrl}${path.relative(__dirname, path.join(__dirname, file))}`)
+  .replace(
+    /(!\[.+])\((\.\/[a-zA-Z/\\.-_0-9]+)\)/g,
+    (match, imageName, imageFile) => `${imageName}(${rawUrl}${path.relative(__dirname, path.join(__dirname, imageFile))})`,
+  );
 fs.writeFileSync(readme, newContent, encoding);
