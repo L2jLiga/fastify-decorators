91e489ef2aba1836ac5922f23d7c0711fde7281c
Andrey Chalkin <l2jliga@gmail.com>
2021-09-07 12:04:12 +0700

    chore: added missed license headers

--- patch-readme.cjs
+++ patch-readme.cjs
@@ -1,3 +1,11 @@
+/**
+ * @license
+ * Copyright Andrey Chalkin <L2jLiga@gmail.com> (https://github.com/L2jLiga). All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/L2jLiga/fastify-decorators/blob/master/LICENSE
+ */
+
 const fs = require('fs');
 const path = require('path').posix;
 
