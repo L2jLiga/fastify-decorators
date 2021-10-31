e9ca752833ad61c63b0bab22f3d4c5b4d9d36f22
L2jLiga <L2jLiga@gmail.com>
2021-10-31 15:47:51 +0200

    chore: refactor patch method

--- plugins/simple-di/src/decorators/helpers/patch-methods.ts
+++ plugins/simple-di/src/decorators/helpers/patch-methods.ts
@@ -30,41 +30,36 @@ function patchHooks(constructor: any): void {
 function patchMethod(constructor: any, methodName: string | symbol): void {
   const _original = constructor.prototype[methodName];
 
-  constructor.prototype[methodName] = function proxiedMethod(request: unknown, reply: unknown, ...rest: unknown[]) {
-    const proxy = new Proxy(this, {
-      get(target, p: string | symbol): unknown {
-        const value = target[p];
-
-        if (hasServiceInjection(value)) {
-          return new Proxy(value, {
-            get(target, p) {
-              if (typeof target[p] === 'function')
-                return target[p].bind(
-                  new Proxy(target, {
-                    get(target, p): any {
-                      if (p === FASTIFY_REQUEST || target[p] === FASTIFY_REQUEST) {
-                        return request;
-                      }
-                      if (p === FASTIFY_REPLY || target[p] === FASTIFY_REPLY) {
-                        return reply;
-                      }
-                      return target[p];
-                    },
-                  }),
-                );
-              return target[p];
-            },
-            has(target, p): boolean {
-              if (p === SERVICE_INJECTION) return false;
-              return p in target;
-            },
-          });
-        }
-
-        return value;
-      },
-    });
-
-    return _original.call(proxy, request, reply, ...rest);
+  constructor.prototype[methodName] = function methodProxy(request: unknown, reply: unknown, ...rest: unknown[]) {
+    return _original.call(createProxy(this, request, reply), request, reply, ...rest);
   };
+}
+
+function createProxy(target: any, request: unknown, reply: unknown): any {
+  return new Proxy(target, {
+    get(target, p) {
+      const value = target[p];
+
+      if (p === FASTIFY_REQUEST || value === FASTIFY_REQUEST) return request;
+      if (p === FASTIFY_REPLY || value === FASTIFY_REPLY) return reply;
+
+      if (hasServiceInjection(value)) {
+        return new Proxy(value, {
+          get(injectedService, prop) {
+            if (prop === FASTIFY_REQUEST || injectedService[prop] === FASTIFY_REQUEST) return request;
+            if (prop === FASTIFY_REPLY || injectedService[prop] === FASTIFY_REPLY) return reply;
+            if (typeof injectedService[prop] === 'function') return injectedService[prop].bind(createProxy(injectedService, request, reply));
+
+            return injectedService[prop];
+          },
+          has(target, p): boolean {
+            if (p === SERVICE_INJECTION) return false;
+            return p in target;
+          },
+        });
+      }
+
+      return value;
+    },
+  });
 }
