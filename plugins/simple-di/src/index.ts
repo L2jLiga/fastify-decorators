af9208c0fff2fcd11ca6221eb14eb0949af7545f
L2jLiga <L2jLiga@gmail.com>
2021-10-31 15:17:02 +0200

    fix: add FastifyInstance to injectables on startup
    fixes issue when controllers inject FastifyInstance by FastifyInstanceToken

--- plugins/simple-di/src/index.ts
+++ plugins/simple-di/src/index.ts
@@ -1,5 +1,12 @@
 import 'reflect-metadata';
 
+import { createInitializationHook } from 'fastify-decorators/plugins';
+import { injectables } from './registry/injectables.js';
+import { FastifyInstanceToken } from './symbols.js';
+import { wrapInjectable } from './utils/wrap-injectable.js';
+
+createInitializationHook('appInit', (fastify) => injectables.set(FastifyInstanceToken, wrapInjectable(fastify)));
+
 export { getInstanceByToken } from './utils/get-instance-by-token.js';
 
 export { Service } from './decorators/service.js';
